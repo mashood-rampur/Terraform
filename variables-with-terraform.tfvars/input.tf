6d5179fb3637bd603c1a573f30762ab008f4e541
Mashood-DevOps <mashood61199@gmail.com>
2025-03-28 12:03:39 +0530

    Create input.tf

---
+++ variables-with-terraform.tfvars/input.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "variable for ec2_instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "variable for ec2_ami_id"
+  type        = string
+}
