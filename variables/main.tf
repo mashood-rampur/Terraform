0a5a0982f4e9a5220964c131dc9fbdd00b8feab5
Mashood-DevOps <mashood61199@gmail.com>
2025-03-28 11:45:36 +0530

    Update main.tf

--- variables/main.tf
+++ variables/main.tf
@@ -1,8 +1,4 @@
 resource "aws_instance" "my-instance" {
-  ami                     = "ami-0e35ddab05955cf57"
+  ami                     = var.ami_id
   instance_type           = var.instance_type
-}
-
-output "public_ip" {
-  value = resource.aws_instance.my-instance.public_ip
 }
