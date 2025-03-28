8e9d613a5ead62609f81b5f3cdb18ae358506ff2
Mashood-DevOps <mashood61199@gmail.com>
2025-03-28 11:15:01 +0530

    Create main.tf

---
+++ variables/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "variable for ec2_instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+resource "aws_instance" "my-instance" {
+  ami                     = "ami-0e35ddab05955cf57"
+  instance_type           = var.instance_type
+}
