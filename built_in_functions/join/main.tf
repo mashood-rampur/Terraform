3149075c21d75ed540a1020ece870e462f26fe9e
Mashood-DevOps <mashood61199@gmail.com>
2025-04-02 18:07:34 +0530

    Create main.tf

---
+++ built_in_functions/join/main.tf
@@ -0,0 +1,8 @@
+variable "my_list" {
+  type    = list
+  default = ["apple", "banana", "cherry"]
+}
+
+output "joined_string" {
+  value = join(", ", var.my_list) # Returns "apple, banana, cherry"
+}
