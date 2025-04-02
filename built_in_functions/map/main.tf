7161a311d9c02b686b17538e90332636eb0eb205
Mashood-DevOps <mashood61199@gmail.com>
2025-04-02 18:06:05 +0530

    Create main.tf

---
+++ built_in_functions/map/main.tf
@@ -0,0 +1,17 @@
+variable "keys" {
+  type    = list
+  default = ["name", "age"]
+}
+
+variable "values" {
+  type    = list
+  default = ["Alice", 30]
+}
+
+output "my_map" {
+value = zipmap(var.keys, var.values)
+}      
+Returns my_map = tomap({
+"age" = "30"
+"name" = "Alice"
+})
