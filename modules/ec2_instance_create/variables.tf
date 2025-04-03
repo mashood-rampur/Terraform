3f95e10822159e31e29ed0a0e5d86f42c03e8df8
Mashood-DevOps <mashood61199@gmail.com>
2025-04-03 12:13:56 +0530

    Create variables.tf

---
+++ modules/ec2_instance_create/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+  description = "variable for instance ami_id"
+  type = "string"
+}
+
+variable "instance_type" {
+  description = "variable for instance_type"
+  type = "string"
+}
