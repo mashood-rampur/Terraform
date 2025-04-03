5515e0a2c6aca2039149e1f01a967ac2a85fb819
Mashood-DevOps <mashood61199@gmail.com>
2025-04-03 12:10:57 +0530

    Delete modules directory

--- modules/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ami_id" {
-  description = "variable for instance ami_id"
-  type = "string"
-}
-
-variable "instance_type" {
-  description = "variable for instance_type"
-  type = "string"
-}
